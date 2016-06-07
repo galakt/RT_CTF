2eafa0f2ae00e9995bb369e58bbec919dd695c1b
Vadim Galaktionov <gala-vadim@narod.ru>
2016-06-07 18:00:06 +0300

    Add solution to issue

---
+++ backdoor_beginners/browser 70/browser 70/browser 70/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace browser_70
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("SDSLabs", "1"));
+            var str = client.GetStringAsync(new Uri(@"http://hack.bckdr.in/BRWSR/"));
+            var result = str.Result;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
